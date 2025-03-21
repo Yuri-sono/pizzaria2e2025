14e5779748314bb83b6268519ca4100c6a627000
Yuri-Sono <rm95269@estudante.fieb.edu.br>
2025-03-21 09:40:10 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+function App(){
+
+  const [nome, setNome] = useState('Ronaldo')
+
+  return (
+      <div>
+        <h3>Pizaaria 2E</h3>
+      
+        <input 
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type="text" />
+
+        <button
+          className="botao"
+          onClick={()=>{alert(nome)}}
+        >
+          CLIQUE AQUI
+        </button>
+      
+      </div>
+  )
+}
+
+export default App
