6faaa1af05327955ad859e0bdc0198bf3e445195
yuri-paz <rm95269@estudante.fieb.edu.br>
2025-09-22 08:27:50 -0300

    cadastro

---
+++ src/pages/produtos/cadastro.jsx
@@ -0,0 +1,23 @@
+import './produto.css'
+const CadastroProduto =() => {
+
+    return (
+        <>
+            <h3>Cadastro de Produtos</h3>
+
+            <input type= "Text" id="id"/>
+            <input type= "Text" id="nome"/>
+            <input type= "Text" id="descricao"/>
+            <input type= "Text" id="preco"/>
+            <input type= "Text" id="tipo"/>
+            <input type= "Text" id="CategoriaID"/>
+            
+            
+                        
+
+        </>
+    )
+
+}
+
+export default CadastroProduto
--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,39 +1,26 @@
 import axios from 'axios'
-import { useEffect,useState } from 'react';
-
+import { useEffect, useState } from 'react';
+ 
 const Produtos = () => {
+    //Estado para guardar a lista de pizzas
+    const [pizzas,setPizzas] = useState(['Calabreza','Muçarela']);
+   // Consumir as pizzas da rota do backend
+   axios.get("")
+   //Consumir Pizzas da rota do backend
+   axios.get("https://172.19.0.49/pizzariaoficial/api/v1/produto") 
+   .then(response => setPizzas(response.data.data))
+   .catch(error => console.log(error))
 
-    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
-    .then((response) => {
-        alert(JSON.stringify(response));
-    })
-    //Consumir os produtos do endpoint(rota)
-    useEffect =(() => {
-
-
-
-    }, []);
-    //Objetos que possui lista de pizzas
-    const pizzas = [
-        "Pizza de Frango",
-        "Pizza de Muçarela",
-        "Pizza de Calabresa",
-        "Pizza Baiana",
-        "Pizza Portuguesa",
-        "Pizza de Atum", "Pizza de Seis Queijos"
-];
-//Mapeamento das pizzas da lista(iteração)
-    const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
-
-
+    // Mapeamento das pizzas da lista (iteração)
+    const listaPizzas = pizzas.map(pizza =>
+        <li key={pizza}>{pizza.nome}</li>);
     return(
-        <>
+    <>
         <h3>Listagem de Produtos</h3>
-      <ul>
-          {listaPizzas}  
-      </ul>
-        </>
+        <ul>
+            {listaPizzas}
+        </ul>
+    </>
     )
 }
-
 export default Produtos
